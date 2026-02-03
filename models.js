e9a166e9d5e23fa67a528800b3a7d1fa12a1ca3f
ssantamaria206 <ssantamaria@institutmvm.cat>
2026-02-03 12:24:56 +0100

    Metodos y Views privados arreglados

--- models.js
+++ models.js
@@ -1,19 +1,21 @@
 class Multimedia {
+    #calcularCostServer;
+    #Views;
     constructor(Titol, Durada) {
         this.Titol = Titol
         this.Durada = Durada
         this.#Views = 0
         }
     }
-    function Play(self) {
+    Play(self);{
         print("Reproduint...")
-        self.Views += 1
+        self.#Views += 1;
     }
 
-    function getViews(self){
-        return self.#Views
+    getViews(self);{
+        return self.#Views;
     }
 
-    function #calcularCostServer(self,Durada){
-        return (Durada * 0.01)
+    #calcularCostServer(self,Durada);{
+        return (Durada * 0.01);
     }

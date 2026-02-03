5befbf705b1a32eb5f1f056d440da41134095900
ssantamaria206 <ssantamaria@institutmvm.cat>
2026-02-03 12:47:16 +0100

    puntos y comas arreglados

--- models.js
+++ models.js
@@ -2,20 +2,21 @@ class Multimedia {
     #calcularCostServer;
     #Views;
     constructor(Titol, Durada) {
-        this.Titol = Titol
-        this.Durada = Durada
-        this.#Views = 0
+        this.Titol = Titol;
+        this.Durada = Durada;
+        this.#Views = 0;
         }
-    }
-    Play(self);{
-        print("Reproduint...")
+    
+    Play(self){
+        console.log("Reproduint...")
         self.#Views += 1;
-    }
+    };
 
-    getViews(self);{
+    getViews(self){
         return self.#Views;
-    }
+    };
 
-    #calcularCostServer(self,Durada);{
-        return (Durada * 0.01);
-    }
+    #calcularCostServer(self,Durada){
+        return (Durada * 0.01)
+    };
+};
